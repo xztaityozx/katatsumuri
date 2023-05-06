ffda71ca7701385f8616c18ab0c55045994d78d9
xztaityozx <taityonohibi@gmail.com>
2023-05-06 15:35:41 +0900

    chore: 生成テスト用のクラス

--- perl/lib/My/Namespace/B.pm
+++ perl/lib/My/Namespace/B.pm
@@ -3,6 +3,16 @@ use strictures 2;
 use Function::Parameters;
 use Function::Return;
 use Types::Standard -types;
+use Data::Validator;
+
+use Mouse;
+
+has name => (is => 'ro', isa => Str, default => 'this is name');
+has age => (is => 'ro', isa => Int, required => 1);
+has union => (is => 'ro', isa => Str|Int, required => 1);
+
+no Mouse;
+__PACKAGE__->meta->make_immutable;
 
 sub a {
     return 10;
@@ -18,6 +28,24 @@ method c() :Return(Int) {
 
 method d(Str $str, $x, Int $y //= 1) {
     return 10;
+};
+
+sub e {
+    my $rule = Data::Validator->new(
+        str => { isa => Str },
+        x => { isa => Any },
+        y => { isa => Int, default => 1 },
+    );
+
+    $rule->validate(@_);
+
+    return 10;
+}
+
+sub f :Return(Str, Int) {
+    my ($self, $str, $x, $y) = @_;
+
+    return [$str, $x+$y];
 }
 
 1;
