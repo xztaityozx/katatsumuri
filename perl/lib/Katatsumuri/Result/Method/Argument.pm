ef5f1ac030e29c4f52835b100044736005437015
xztaityozx <taityonohibi@gmail.com>
2023-05-18 22:31:20 +0900

    chore: Jsonのbool値になってなかった

--- perl/lib/Katatsumuri/Result/Method/Argument.pm
+++ perl/lib/Katatsumuri/Result/Method/Argument.pm
@@ -1,6 +1,6 @@
 package Katatsumuri::Result::Method::Argument;
 use strictures 2;
-use Types::Standard qw( Str HashRef Any Undef Bool InstanceOf Dict );
+use Types::Standard qw( Str HashRef Any Undef Bool InstanceOf Dict ScalarRef );
 use Moo;
 use Function::Parameters;
 use Function::Return;
@@ -9,11 +9,11 @@ has name     => (is => 'ro', isa => Str, required => 1);
 has type     => (is => 'ro', isa => InstanceOf['Katatsumuri::Type'], required => 1);
 has required => (is => 'ro', isa => Bool, required => 1);
 
-method to_schema ($class :) : Return(Dict[name => Str, type => HashRef, required => Bool]) {
+method to_schema ($class :) : Return(Dict[name => Str, type => HashRef, required => ScalarRef]) {
     return +{
         name     => $class->name,
         type     => $class->type->to_json_schema(),
-        required => $class->required
+        required => $class->required ? \1 : \0
     };
 }
 
